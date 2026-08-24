8ae70878a1121be0cb794c9a73f37f89cb4b0961
Mazex-dz <malekzer2007@gmail.com>
2026-08-23 22:17:41 -0700

    Update clinic website

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DemoOne from "@/components/demo";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DemoOne />
-    </main>
-  );
-}
